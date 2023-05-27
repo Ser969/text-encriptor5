300becfe5ea3492bdef5e772a6a1bc032616b909
Sergio <Sergio>
2023-05-26 22:37:29 -0600

    Diseño de la aplicacion con css y con la funcionalidad de encriptar un texto

---
+++ index.js
@@ -0,0 +1,32 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo")
+    let titere = document.getElementById("titere");
+
+    // La letra "e" es convertida para "enter"
+    // La letra "i" es convertida para "imes"
+    // La letra "a" es convertida para "ai"
+    // La letra "o" es convertida para "ober"
+    // La letra "u" es convertida para "ufat"
+
+
+    let textoCifrado = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/u, "ufat");
+
+   if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto encriptado con exito";
+      parrafo.textContent = "";
+      titere.src = "./img/encriptado.jpg";
+ } else {
+    titere.src = "./img/Muñeco.png";
+    tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    alert("Debes ingresar algun texto");
+ }
+}
